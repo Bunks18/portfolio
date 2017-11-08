959c74790cbdef93a68806b179baf25efb24d89b
Bunks18 <wbunker11@gmail.com>
2017-11-07 20:48:42 -0500

    added bio and skills, made it mobile friendly

--- src/components/Test.js
+++ src/components/Test.js
@@ -7,17 +7,33 @@ class Test extends Component {
     return (
       <div className="back">
         <div className="white-bottom">
-        <h2>Will Bunker</h2>
-        <h4>Washington, DC</h4>
-        <h4>Technical Consultant</h4>
-        <SocialIcon className="icon" url="https://www.linkedin.com/in/will-bunker-428b9155/" />
-        <SocialIcon className="icon" url="https://github.com/Bunks18/"/>
+          <h2>Will Bunker</h2>
+          <h4>Washington, DC</h4>
+          <h4>Technical Consultant</h4>
+          <SocialIcon
+            className="icon"
+            url="https://www.linkedin.com/in/will-bunker-428b9155/"
+          />
+          <SocialIcon className="icon" url="https://github.com/Bunks18/" />
         </div>
         <div>
-        <h2>About</h2>
-       <h4>
-       My name is Will and this is my portfolio site. I'm a technology consultant by day and an aspiring web developer when I'm not doing that.
-       </h4> 
+          <div className="about">
+            <div className="about-child section-1">
+            <h3>About</h3>
+            <h4>I'm Will and I'm a technology consultant by day and</h4>
+            <h4>an aspiring web developer when I'm not doing that.</h4>
+            <h4>I love learning, building things I don't how to build, and I thrive in a team environment.</h4>
+
+            </div>
+            <div className="about-child">
+            <h3>Skills</h3>
+            <h4>
+            <h4>Languages: Javascript · HTML · CSS · R</h4>
+            <h4>Front-End: React · Redux · Plotly · jQuery</h4>
+            <h4>Back-End: Node · Express · Mongo · SQL · Socket.io</h4>
+            </h4>
+            </div>
+          </div>
         </div>
         <div className="content">
           <div className="grid">
@@ -37,43 +53,45 @@ class Test extends Component {
               </figcaption>
             </figure>
             <figure className="effect-goliath">
-            <img src={require('../tt.PNG')} alt="img23" />
-            <figcaption>
-              <p>
-                A gift to my father. His very own travel consulting services website. Built with an html template purchased from Themeforest for $17.
-              </p>
-              <a
-                target="_blank"
-                href="http://tailoredtravels.co/index.html"
-              >
-                View more
-              </a>
-            </figcaption>
-          </figure>
-          <figure className="effect-goliath">
-          <img src={require('../goal.PNG')} alt="img23" />
-          <figcaption>
-            <p>
-              My first full stack application. A labor of love that resulted in an ugly baby. Built with Mongo, Node, Express, React, & Redux.
-            </p>
-            <a
-              target="_blank"
-              href="https://cryptic-brushlands-72828.herokuapp.com/landing"
-            >
-              View more
-            </a>
-          </figcaption>
-        </figure>
-        <figure className="effect-goliath">
-        <img src={require('../slack.PNG')} alt="img23" />
-        <figcaption>
-          <p>
-            My pride and joy. Coming soon...a slack clone, but way worse. Built on the MERN stack with socket.io.
-          </p>
-        </figcaption>
-      </figure>
+              <img src={require('../tt.PNG')} alt="img23" />
+              <figcaption>
+                <p>
+                  A gift to my father. His very own travel consulting services
+                  website. Built with an html template purchased from
+                  Themeforest for $17.
+                </p>
+                <a target="_blank" href="http://tailoredtravels.co/index.html">
+                  View more
+                </a>
+              </figcaption>
+            </figure>
+            <figure className="effect-goliath">
+              <img src={require('../goal.PNG')} alt="img23" />
+              <figcaption>
+                <p>
+                  My first full stack application. A labor of love that resulted
+                  in an ugly baby. Built with Mongo, Node, Express, React, &
+                  Redux.
+                </p>
+                <a
+                  target="_blank"
+                  href="https://cryptic-brushlands-72828.herokuapp.com/landing"
+                >
+                  View more
+                </a>
+              </figcaption>
+            </figure>
+            <figure className="effect-goliath">
+              <img src={require('../slack.PNG')} alt="img23" />
+              <figcaption>
+                <p>
+                  My pride and joy. Coming soon...a slack clone, but way worse.
+                  Built on the MERN stack with socket.io.
+                </p>
+              </figcaption>
+            </figure>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
